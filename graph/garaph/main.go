efda7ced0be4a81215759c2e4a42519553f1cfab
FASIL <fasilkp2000@gmail.com>
2023-02-09 19:26:55 +0530

    graph
    complete graph BFS and DFS functions

--- graph/garaph/main.go
+++ graph/garaph/main.go
@@ -17,6 +17,10 @@ func main() {
 	graph.insert(3, 4, true)
 	graph.insert(5, 6, false)
 
+	bfs := graph.BFS(3)
+	dfs := graph.DFS(3)
+	fmt.Println(bfs, "\n", dfs)
+
 	graph.display()
 
 }
@@ -51,4 +55,61 @@ func (g *Graph) addVertex(vertex int) {
 func (g *Graph) exist(vertex int) bool {
 	_, exist := g.table[vertex]
 	return exist
+}
+
+func (g *Graph) BFS(start int) []int {
+	queue := []int{start}
+	visited := make(map[int]bool)
+	visited[start] = true
+	result := []int{}
+
+	for len(queue) > 0 {
+
+		vertex := queue[0]
+		result = append(result, vertex)
+		queue = queue[1:]
+		for _, v := range g.table[vertex] {
+
+			if !visited[v] {
+				visited[v] = true
+				queue = append(queue, v)
+			}
+		}
+	}
+
+	for key := range g.table {
+
+		if !visited[key] {
+			g.BFS(key)
+		}
+	}
+	return result
+}
+
+func (g *Graph) DFS(start int) []int {
+	visited := make(map[int]bool)
+	result := []int{start}
+
+	var DFSUTIL func(vertex int)
+	DFSUTIL = func(vertex int) {
+
+		visited[vertex] = true
+		for _, v := range g.table[vertex] {
+
+			if !visited[v] {
+
+				DFSUTIL(v)
+				result = append(result, v)
+			}
+		}
+
+	}
+
+	DFSUTIL(start)
+	for key := range g.table {
+		if !visited[key] {
+			g.DFS(key)
+		}
+	}
+	return result
 }
